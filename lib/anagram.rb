03707931ae623d299f773db85e77e3f230652d9d
Kish Siva <rodes88@msn.com>
2019-04-25 02:34:55 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize (word)
+    @word = word 
+  end 
+  
+  def match(list)
+    list.select do |element|
+      #binding.pry
+        @word.chars.sort.join("") == element.chars.sort.join("")
+    end
+  end
+  
+end 

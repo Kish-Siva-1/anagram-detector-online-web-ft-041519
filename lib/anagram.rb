ea0784feafbdb2b437c2ab95122db5992a6f6dd2
Kish Siva <rodes88@msn.com>
2019-04-25 02:34:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,10 +9,10 @@ class Anagram
   end 
   
   def match(list)
-    [list.select do |element|
+    list.select do |element|
       #binding.pry
         @word.chars.sort.join("") == element.chars.sort.join("")
-    end] 
+    end
   end
   
 end 
